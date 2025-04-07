59006307504581fd6f4f261205c86359bbbdd8e1
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-04-07 13:58:23 +0100

    added basic javascript
    added typewriter function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "You may write me down in history With your bitter, twisted lies, You may trod me in the very dirt But still, like dust, I'll rise.",
+    autoStart: true,
+  });
+}
+
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
