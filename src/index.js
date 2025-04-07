825e6470b70e3aec2e65dd18685fbc6c17d36584
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-04-07 17:43:51 +0100

    added AI API and axios
    corrected the JS so it is functioning correctly

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+function displayPoem(response) {
+  console.log("poem generated");
+
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings:
-      "You may write me down in history With your bitter, twisted lies, You may trod me in the very dirt But still, like dust, I'll rise.",
-    autoStart: true,
-  });
+  let instructionsInput = document.querySelector(".poem-input");
+  let apiKey = "16bfc20460o66fe0f260tfe95532783a";
+  let prompt = `generate a poem about ${instructionsInput.value}`;
+  let context =
+    "you are a poem expert. please generate a four line poem, each line starting on a new line.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  console.log("generating poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-form");
